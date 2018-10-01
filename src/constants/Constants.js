3547e1768035ca302daffb62385d43816842d217
Quinn Andrews <quinn1971@gmail.com>
2018-09-30 17:02:54 -0700

    Added a Constants class for convenience.

---
+++ src/constants/Constants.js
@@ -0,0 +1,12 @@
+export const listTab = 'listTab';
+export const editTab = 'editTab';
+export const helpTab = 'helpTab';
+
+export const listTabPane = 'listTabPane';
+export const editTabPane = 'editTabPane';
+export const helpTabPane = 'helpTabPane';
+
+export const listTabLabel = 'FIND';
+export const editTabCreateLabel = 'CREATE';
+export const editTabEditLabel = 'EDIT';
+export const helpTabLabel = 'HELP';
