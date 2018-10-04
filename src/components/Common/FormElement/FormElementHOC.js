92e2ca2362ac0e289da7a67dee7e9938149ae1aa
Quinn Andrews <quinn1971@gmail.com>
2018-10-04 15:35:00 -0700

    Made an HOC version of FormElement.

---
+++ src/components/Common/FormElement/FormElementHOC.js
@@ -0,0 +1,135 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import Converter from "../../../converters/Converter";
+import Validator from "../../../validators/Validator";
+import RequiredValidator from "../../../validators/RequiredValidator";
+
+//const myHOC = (WrappedComponent) => {};
+
+export default function formElement(WrappedComponent) {
+
+    return class FormElement extends Component {
+
+        validators = [];
+        converters = [];
+
+        constructor(props) {
+            super(props);
+            this.state = {
+                valid: true,
+                validationFeedback: ''
+            };
+            if (this.props.required) {
+                this.addValidator(new RequiredValidator());
+            }
+            this.addConverter(this.props.converter);
+            this.addValidator(this.props.validator);
+            this.addConverters(this.props.converters);
+            this.addValidators(this.props.validators);
+        }
+
+        isValid() {
+            return this.state.valid;
+        }
+
+        setValid(valid) {
+            this.setState({valid: valid});
+        }
+
+        getValidationFeedback() {
+            return this.state.validationFeedback;
+        }
+
+        setValidationFeedback(feedback) {
+            this.setState({validationFeedback: feedback});
+        }
+
+        getClassName() {
+            return "form-control" + (this.state.valid ? '' : ' is-invalid');
+        }
+
+        addConverter(converter) {
+            if (converter !== undefined) {
+                this.converters.push(converter);
+            }
+        }
+
+        addConverters(converters) {
+            if (converters !== undefined) {
+                for (const c of converters) {
+                    this.addConverter(c);
+                }
+            }
+        }
+
+        addValidator(validator) {
+            if (validator !== undefined) {
+                this.validators.push(validator);
+            }
+        }
+
+        addValidators(validators) {
+            if (validators !== undefined) {
+                for (const v of validators) {
+                    this.addValidator(v);
+                }
+            }
+        }
+
+        convert(event) {
+            let value = event.target.value;
+            for (const c of this.converters) {
+                value = c.convert(value)
+            }
+            event.target.value = value;
+        }
+
+        validate(event) {
+            let value = this.props.value;
+            if (event !== undefined) {
+                value = event.target.value;
+            }
+            let valid = true;
+            for (const v of this.validators) {
+                if (!v.validate(value)) {
+                    valid = false;
+                    this.setValidationFeedback(v.getMessage(this.props.label));
+                    break;
+                }
+            }
+            console.log("valid: " + valid);
+            this.setValid(valid);
+            return valid;
+        }
+
+        valueChangeHandler = (event) => {
+            this.convert(event);
+            this.validate(event);
+            this.props.valueChangeHandler(event);
+        };
+
+        render() {
+            const {valueChangeHandler, ...filteredProps} = this.props;
+            return (
+                <WrappedComponent {...filteredProps}
+                                  valueChangeHandler={this.valueChangeHandler}
+                                  isValid={this.isValid()}
+                                  validationFeedback={this.getValidationFeedback()}
+                                  className={this.getClassName()}/>
+            );
+        }
+
+    };
+
+}
+
+
+formElement.propTypes = {
+    label: PropTypes.string,
+    converter: PropTypes.instanceOf(Converter),
+    validator: PropTypes.instanceOf(Validator),
+    converters: PropTypes.arrayOf(Converter),
+    validators: PropTypes.arrayOf(Validator),
+    valueChangeHandler: PropTypes.func
+};
+
